aad4a1701c332e66ad6e9ceaa89bc4e34115187d
ae95caba <ae95caba@gmail.com>
2024-10-09 12:25:33 -0300

    feat: :sparkles: handle errors in "ingreso" page

--- src/app/miembros/[id]/page.tsx
+++ src/app/miembros/[id]/page.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-async function getUser(id) {
-  const res = await fetch(`http://localhost:3000/api/users/${id}`);
+export async function getUser(id) {
+  try {
+    const res = await fetch(`http://localhost:3000/api/users/${id}`);
 
-  const data = await res.json();
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.error || "An error occurred"); // Throw an error with the response message
+    }
+    const data = await res.json();
 
-  return data;
+    return data;
+  } catch (error) {
+    console.log(error.message);
+    alert(error.message);
+  }
 }
 export default async function User({ params }) {
   const user = await getUser(params.id);
