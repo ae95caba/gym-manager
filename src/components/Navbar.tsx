8ae401a6c276e76538934aef113d5948d05cf7f6
ae95caba <ae95caba@gmail.com>
2024-10-11 00:44:24 -0300

    feat: :sparkles: make navbar links change style when active

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,34 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation"; // Correct import
 const Navbar = () => {
+  const pathname = usePathname(); // Get the current pathname
+  const links = [
+    { href: "/miembros", label: "Miembros" },
+    { href: "/alta", label: "Alta" },
+    { href: "/ingreso", label: "Ingreso" },
+    { href: "/salida", label: "Salida" },
+  ];
+
   return (
-    <nav className="flex justify-between p-5">
+    <nav className="flex justify-between p-5 bg-slate-900">
       <div className="navbar-logo">
         <h1>MyApp</h1>
       </div>
       <ul className="flex gap-10 ">
-        <li>
-          <Link href="/miembros">Miembros</Link>
-        </li>
-        <li>
-          <Link href="/alta">Alta</Link>
-        </li>
-        <li>
-          <Link href="/ingreso">Ingreso</Link>
-        </li>
-        <li>
-          <Link href="/salida">Salida</Link>
-        </li>
+        {links.map(({ href, label }) => (
+          <li key={href}>
+            <Link
+              href={href}
+              className={
+                pathname === href ? "text-blue-500 font-bold" : "text-white"
+              }
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
       </ul>
       <div className="navbar-actions">
         <button className="btn-login">Login</button>
